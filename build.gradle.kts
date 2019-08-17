5af372f6f3443fd0fc8a51252b7d9d89204588cc
Matt Moore <mattmoore@carbonhelix.com>
2019-08-17 17:08:04 -0500

    Bintray publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTargetPreset
 
 plugins {
     kotlin("multiplatform") version "1.3.41"
+    id("com.jfrog.bintray") version "1.8.4"
 }
 
 repositories {
@@ -32,6 +33,27 @@ kotlin {
         }
         val linuxX64Main by getting {
             kotlin.srcDir("src/nativeMain/kotlin")
+        }
+    }
+}
+
+bintray {
+    user = "mattmoore"
+    key = "498e6f3347faa4b02f6e8be98a70f6afbf926e96"
+    publish = true
+//    setPublications("")
+    with(pkg) {
+        repo = "io.mattmoore"
+        name = "png"
+//        userOrg = "mattmoore"
+        websiteUrl = "https://github.com/mattmoore/kotlin-png"
+        vcsUrl = "https://github.com/mattmoore/kotlin-png"
+        setLabels("kotlin")
+        setLicenses("MIT")
+        with(version) {
+            name = project.name
+            desc = project.description
+            vcsTag = project.version.toString()
         }
     }
 }
